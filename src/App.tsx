e582d470e84d133d8b40f30b469891f85eb49826
Actlam <stream.actlam@gmail.com>
2022-03-28 20:06:43 +0900

    add: 間違えて消していたimportをもとに戻す

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import Header from './components/Header'
 import AggregationPeriodButtons from './components/AggregationPeriodButtons'
+import { ChakraProvider } from '@chakra-ui/react'
 
 const App = () => {
     const [selectedPeriod, setSelectedPeriod] = useState('daily')
